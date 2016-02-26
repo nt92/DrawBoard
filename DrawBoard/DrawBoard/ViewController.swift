0520e4c4a69bfe8521788ce44a5035938b864491
Nikhil Thota <nt92@ufl.edu>
2016-02-25 19:52:48 -0500

    added more animations

--- DrawBoard/DrawBoard/ViewController.swift
+++ DrawBoard/DrawBoard/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    
+    @IBAction func onTrayPanGestures(panGestureRecognizer: UIPanGestureRecognizer) {
+        let parentView = view
+        // Absolute (x,y) coordinates in parent view's coordinate system
+        let point = panGestureRecognizer.locationInView(parentView)
+        
+        // Total translation (x,y) over time in parent view's coordinate system
+        let translation = panGestureRecognizer.translationInView(parentView)
+        
+        var trayOriginalCenter = trayView.center
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            trayOriginalCenter = trayView.center
+            print("Gesture began at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: point.y)
+            print("Gesture changed at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
